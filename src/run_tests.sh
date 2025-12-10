258cd7410d0bf155ccabff412fa6338d46ce7015
Emmanuel Hameyie <hamsman@gmail.com>
2025-12-09 23:17:21 -0500

    remove old docs

--- src/run_tests.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /Users/hamsman/Dev/PdfiumWrapper/src
-echo "Running PdfFormTests..."
-dotnet test --filter "FullyQualifiedName~PdfFormTests" --logger "console;verbosity=normal"
-echo "Exit code: $?"
-
